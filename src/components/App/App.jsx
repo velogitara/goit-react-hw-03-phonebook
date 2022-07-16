a776654af46032a1e8d630772091a31e48b3a664
Oleksandr Pavlishchev <velogitara@gmail.com>
2022-07-17 00:36:06 +0300

    fix bag

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -13,9 +13,12 @@ class App extends Component {
   componentDidMount() {
     const savedContacts = localStorage.getItem('contacts');
     const parsedContacts = JSON.parse(savedContacts);
-    this.setState({
-      contacts: parsedContacts,
-    });
+
+    if (parsedContacts) {
+      this.setState({
+        contacts: parsedContacts,
+      });
+    }
   }
   componentDidUpdate(prevProps, prevState) {
     if (prevState.contacts !== this.state.contacts) {
